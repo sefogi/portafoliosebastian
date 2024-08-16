a1677aef2398746ba34ad6c00b6e6d08b9f61940
sefogi <111927416+sefogi@users.noreply.github.com>
2024-08-16 03:41:21 +0200

    Update contact.php
    up2

--- forms/contact.php
+++ forms/contact.php
@@ -1,27 +1,36 @@
 <?php
-  
-  
-  $receiving_email_address = 'sebast18@gmail.com';
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
+$receiving_email_address = 'sebast18@gmail.com';
 
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
-  
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
+// Verificar que el archivo php-email-form.php existe
+if (file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php')) {
+    include($php_email_form);
+} else {
+    die('Unable to load the "PHP Email Form" Library!');
+}
 
+$contact = new PHP_Email_Form;
+$contact->ajax = true;
 
+// Configuración de la dirección de correo
+$contact->to = $receiving_email_address;
+$contact->from_name = isset($_POST['name']) ? strip_tags($_POST['name']) : '';
+$contact->from_email = isset($_POST['email']) ? strip_tags($_POST['email']) : '';
+$contact->subject = isset($_POST['subject']) ? strip_tags($_POST['subject']) : '';
 
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
+// Validaciones de entrada
+if (empty($contact->from_name) || empty($contact->from_email) || empty($contact->subject)) {
+    die('Por favor, completa todos los campos requeridos.');
+}
 
-  echo $contact->send();
+// Agregar los mensajes al correo
+$contact->add_message($contact->from_name, 'From');
+$contact->add_message($contact->from_email, 'Email');
+$contact->add_message(isset($_POST['message']) ? strip_tags($_POST['message']) : '', 'Message', 10);
+
+// Enviar el formulario y mostrar el resultado
+echo $contact->send();
+
+?>
+
 
